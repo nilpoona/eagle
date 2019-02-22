cd309dfebfcbc3fc4633e424ffaf4c57d95e8536
nilpoona <fantasyrock_0356@yahoo.co.jp>
2019-02-23 02:46:22 +0900

    bind

--- helper.go
+++ helper.go
@@ -13,6 +13,11 @@ func PathParam(r *http.Request, k string) string {
 	return ""
 }
 
+func Bind(r *http.Request, v interface{}) error {
+	enc := json.NewDecoder(r.Body)
+	return enc.Decode(v)
+}
+
 func RenderJSON(w http.ResponseWriter, code int, v interface{}) error {
 	w.Header().Set("Content-Type", "application/json; charset=utf-8")
 	w.WriteHeader(code)
