66af79c455e12fed6ac1082dc36eb4fe0bf54be5
nilpoona <fantasyrock_0356@yahoo.co.jp>
2019-02-26 10:51:40 +0900

    cors middleware

--- helper.go
+++ helper.go
@@ -5,6 +5,13 @@ import (
 	"net/http"
 )
 
+var (
+	DefaultCORSHeaders = CORSHeaders{
+		AllowOrigins: []string{"*"},
+		AllowMethods: []string{http.MethodGet, http.MethodHead, http.MethodPut, http.MethodPatch, http.MethodPost, http.MethodDelete},
+	}
+)
+
 func PathParam(r *http.Request, k string) string {
 	if v := r.Context().Value(pathParamKey(k)); v != nil {
 		return v.(string)
@@ -16,6 +23,20 @@ func PathParam(r *http.Request, k string) string {
 func Bind(r *http.Request, v interface{}) error {
 	enc := json.NewDecoder(r.Body)
 	return enc.Decode(v)
+}
+
+type CORSHeaders struct {
+	AllowOrigins     []string `json:"Access-Control-Allow-Origin"`
+	AllowMethods     []string `json:"Access-Control-Allow-Methods"`
+	AllowHeaders     []string `json:"Access-Control-Allow-Headers"`
+	ExposeHeaders    []string `json:"Access-Control-Expose-Headers"`
+	MaxAge           uint     `json:"Access-Control-Max-Age"`
+	AllowCredentials bool     `json:"Access-Control-Allow-Credentials"`
+}
+
+func SetCORSHeaders(ch CORSHeaders) error {
+
+	return nil
 }
 
 func RenderJSON(w http.ResponseWriter, code int, v interface{}) error {
---
+++ middleware.go
@@ -0,0 +1,81 @@
+package eagle
+
+import (
+	"net/http"
+	"strings"
+)
+
+func CORS(config CORSHeaders) Middleware {
+	allowOrigins := config.AllowOrigins
+	if len(allowOrigins) == 0 {
+		allowOrigins = DefaultCORSHeaders.AllowOrigins
+	}
+
+	allowMethods := config.AllowMethods
+	if len(allowMethods) == 0 {
+		allowMethods = DefaultCORSHeaders.AllowMethods
+	}
+
+	am := strings.Join(allowMethods, ",")
+	ah := strings.Join(config.AllowHeaders, ",")
+	eh := strings.Join(config.ExposeHeaders, ",")
+	ma := config.MaxAge
+	ac := config.AllowCredentials
+
+	return func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+			origin := r.Header.Get("Origin")
+			allowOrigin := ""
+
+			for _, o := range allowMethods {
+				if o == "*" || ac {
+					allowOrigin = o
+					break
+				}
+
+				if o == "*" || o == origin {
+					allowOrigin = o
+					break
+				}
+			}
+
+			if r.Method != http.MethodOptions {
+				w.Header().Add("Vary", "Origin")
+				w.Header().Set("Access-Control-Allow-Origin", allowOrigin)
+				if ac {
+					w.Header().Set("Access-Control-Allow-Credentials", "true")
+				}
+				if eh != "" {
+					w.Header().Set("Access-Control-Expose-Headers", eh)
+				}
+
+				next.ServeHTTP(w, r)
+				return
+			}
+
+			w.Header().Add("Vary", "Origin")
+			w.Header().Add("Vary", "Access-Control-Request-Method")
+			w.Header().Add("Vary", "Access-Control-Request-Headers")
+			w.Header().Add("Access-Control-Allow-Methods", am)
+			w.Header().Add("Access-Control-Allow-Origin", allowOrigin)
+			if ma > 0 {
+				w.Header().Set("Access-Control-Max-Age", string(ma))
+			}
+
+			if ac {
+				w.Header().Set("Access-Control-Allow-Credentials", "true")
+			}
+
+			if ah != "" {
+				w.Header().Set("Access-Control-Allow-Headers", ah)
+			} else {
+				h := r.Header.Get("Access-Control-Request-Headers")
+				if h != "" {
+					w.Header().Set("Access-Control-Allow-Headers", h)
+				}
+			}
+
+			w.WriteHeader(http.StatusNoContent)
+		}
+	}
+}
